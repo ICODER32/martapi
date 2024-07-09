2defcb62424319fa6c8ca2bb711d922c88347eaa
Ibtisam Anwar <ibtisamanwar008@gmail.com>
2024-07-09 20:52:30 +0500

    improved login

--- user_service/main.py
+++ user_service/main.py
@@ -4,7 +4,7 @@ from db import create_db_and_tables,engine,User,UserCreate,get_session
 from sqlmodel import Session,select
 from pydantic import BaseModel
 import json
-
+from bcrypt import hashpw, gensalt
 import logging
 from jose import JWTError, jwt
 logging.basicConfig(level=logging.INFO)
@@ -35,10 +35,12 @@ def read_root():
 @app.post("/register/")
 async def create_user(user: UserCreate, session: Session = Depends(get_session)):
     loggger.info(f"Creating user {user}")
+    hashed_password = hashpw(user.password.encode("utf-8"), gensalt())
+
     user = User(
         name=user.name,
         email=user.email,
-        password=user.password,
+        password=hashed_password.decode("utf-8"),
         role=user.role
     )
     session.add(user)
@@ -53,18 +55,22 @@ async def create_user(user: UserCreate, session: Session = Depends(get_session))
 @app.post("/login")
 async def login(user: UseLogin, session: Session = Depends(get_session)):
     loggger.info(f"Logging in user {user}")
-    user = session.exec(select(User).where(User.email == user.email)).first()
-    if user is None:
+    isUser = session.exec(select(User).where(User.email == user.email)).first()
+    if isUser is None:
         raise HTTPException(status_code=404, detail="User not found")
-    if user.password != user.password:
+    
+    
+    if not hashpw(user.password.encode("utf-8"), isUser.password.encode("utf-8")) == isUser.password.encode("utf-8"):
         raise HTTPException(status_code=401, detail="Invalid password")
+    
+    
     
     # create jwt token
     user_data = {
-        "user_id": user.id,
-        "name": user.name,
-        "email": user.email,
-        "role": user.role
+        "id": isUser.id,
+        "name": isUser.name,
+        "email": isUser.email,
+        "role": isUser.role
     }
     token = jwt.encode(user_data, "secret", algorithm="HS256")
     return {
